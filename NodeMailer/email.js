f6e6ff3b84e53638d8d8d78447854e057d643802
dheeraj <dheerajverma2903@gmail.com>
2026-07-16 22:14:09 +0530

    Email Credential fix

--- NodeMailer/email.js
+++ NodeMailer/email.js
@@ -38,22 +38,5 @@ const sendEmail = async (to, subject, text, html) => {
     console.error('Error sending email:', error);
   }
 };
-// const sendEmail = async (to, subject, text, html) => {
-//   try {
-//     const info = await transporter.sendMail({
-//       from: `"Test-Nodemailer" <${process.env.EMAIL_USER}>`, // sender address
-//       to, // list of receivers
-//       subject, // Subject line
-//       text, // plain text body
-//       html, // html body
-//     });
-
-//     console.log('Message sent: %s', info.messageId);
-//     console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
-//   } catch (error) {
-//     console.error('Error sending email:', error);
-//   }
-// };
-
 
 module.exports = {transporter, sendEmail};
